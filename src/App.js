6a9aea042e03c5f057638bbcf241cfa4d1f8c93e
Olsi Korreshi <olsikorreshi@Olsis-MBP.attlocal.net>
2023-02-28 13:59:21 -0600

    using reduce

--- src/App.js
+++ src/App.js
@@ -2,134 +2,72 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const dataRewards = [ 
- 
+  const users =
+[
     {
-      userId: '1u',
-        rewards:[
-           {
-            rewardId: '1r',
-            purchase: 110, 
-            dateTime:'December 17, 1995 03:24:00'
-          },
-          {
-            rewardId: '1r',
-            purchase: 120, 
-            dateTime:'December 17, 1995 03:24:00'
-          },
-          {
-            rewardId: '1r',
-            purchase: 110, 
-            dateTime:'November 17, 1995 03:24:00'
-          },
-         ],
-  },
-      {
-        userId: '2u',
-          rewards:[
-          {
-            rewardId: '1r',
-            purchase: 120, 
-            dateTime:'December 17, 1995 03:24:00'
-          }
-        ],
-      }
-    ]
-    
-    const [rewards, setRewards] = useState([])
-  // useEffect(()=>{
-  //   let fetchRes = fetch(
-  //     "https://jsonplaceholder.typicode.com/todos/1").then(response=> response.json()).then(data=> setRewards(data))
-    
-  //  },[])
-    
-    // setRewards(dataRewards)
-
-    //Evaluate reward pooints
-
-
-    const evaluateRewardPoints = (reward) => {
-      let onePoint =0;
-      let twoPoint = 0;
-    
-      if(reward> 100){
-        twoPoint = (reward-100)*2
-        if(reward >50){
-
-          onePoint = 100 - 50; 
+      "id": "fjaksdfljaskljf",
+      "name": "bosi yzberishtit",
+      "purchases": [
+        {
+          "id": "fjhdskajf",
+          "price": 200,
+          "createdAt": new Date()
         }
-      }else if(reward >50){
-          
-        onePoint = reward - 50; 
-      }
-    
-  return  onePoint + twoPoint 
-    }
-
-    const evaluateRewards = (rewards,evaluateRewardPoints)=>{
-      const userRewards = []
-       userRewards.push(rewards.map(user =>(
-                      {
-                        userId:user.userId, 
-                        rewardPoints:user.rewards.map(reward => (
-                          {
-                          month:reward.dateTime.split(' '),
-                            purchase:evaluateRewardPoints(reward.purchase)
-                          }
-                          ))})))
-                          
-  
-     return userRewards
-
-    }
-    useEffect(()=>{  setRewards(evaluateRewards(dataRewards,evaluateRewardPoints))},[])
-
-   
-    const finalRewards = (rewards) =>{
- 
-      const array = []
-      const unique =[]
-      rewards[0].forEach(item => array.push({userId:item.userId, points:item.rewardPoints.forEach(
-        ele => {
-
-          const month = ele.month[0];
-          const purchase = ele.purchase;
-          
-          const existingMonthIndex = unique.findIndex((item) => item.month === month);
-  
-          if (existingMonthIndex === -1) {
-            unique.push({ month, points: purchase });
-        
-          } else {
-            unique[existingMonthIndex].points += purchase;
-          }
-    
-        // if(unique.length<1){
-
-        //    unique.push({month:ele.month[0],points:ele.purchase})
-        //    console.log(unique)
-        //  }
-        //  else if(unique.filter(item=> !item.month.includes(ele.month[0]))){
-
-        //     unique.push({month:ele.month[0],points:ele.purchase})
-        //     console.log(unique)
-        //   }else{
-        //     unique.points+=ele.purchase
-      
-        //   }
-         
-      
+      ]
+    },
+    {
+      "id": "fjhdskajf",
+      "name": "bosi zogut te zi",
+      "purchases": [
+        {
+          "id": "fjhdskajf",
+          "price": 200,
+            "createdAt": new Date()
+        },
+        {
+          "id": "fjhdskajf",
+          "price": 234,
+					"createdAt": new Date()
         }
-        
-      )}))
-
-      return array
+      ]
     }
-
+  ]
     
-if (rewards.length > 0) {
- console.log(finalRewards(rewards));
+
+
+
+
+
+
+function calculatePoints(price){
+ 		  if (price > 100) {
+      return (price - 100) * 2 + 50;
+    } else if (price > 50) {
+      return price - 50;
+    } else {
+      return 0;
+    }
 }
+
+
+const rewards = users.map(user => {
+  const userId = user.id;
+
+  const rewards = user.purchases.reduce((acc, purchase) => {
+    const month = new Date(purchase.createdAt).getMonth();
+    const year = new Date(purchase.createdAt).getFullYear();
+    const key = `${month}-${year}`;
+    const newPoints = (acc[key] ?? 0) + calculatePoints(purchase.price);
+    return { ...acc, [key]: newPoints };
+  }, {});
+
+  return { userId, rewards };
+});
+
+    
+
+ console.log(rewards);
+
   // return (
   //   <div className="App">
      
